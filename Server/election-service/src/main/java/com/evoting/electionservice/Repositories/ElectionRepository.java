4a1253640e2d29271bb72d10d26b0a53f82a4cdc
Mohamed Bader Moussa <mohamedbader.moussa@esprit.tn>
2022-10-19 15:11:00 +0100

    cleaning files

--- Server/election-service/src/main/java/com/evoting/electionservice/Repositories/ElectionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.evoting.electionservice.Repositories;
-
-import com.evoting.electionservice.Entities.Election;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ElectionRepository extends JpaRepository<Election,Long> {
-}
