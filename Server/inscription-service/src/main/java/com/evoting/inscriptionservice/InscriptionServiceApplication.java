45e15fe662c8464438a996e252e0ae9156433986
Amine Amri <amine.elamri@gmail.com>
2022-10-22 14:41:28 +0100

    eureka server

--- Server/inscription-service/src/main/java/com/evoting/inscriptionservice/InscriptionServiceApplication.java
+++ Server/inscription-service/src/main/java/com/evoting/inscriptionservice/InscriptionServiceApplication.java
@@ -2,12 +2,22 @@ package com.evoting.inscriptionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class InscriptionServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InscriptionServiceApplication.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String home() {
+        return "Service Inscription...";
     }
 
 }
