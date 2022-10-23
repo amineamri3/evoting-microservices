57e8e9cd623187f5e0139e470edc9174094ae56d
Amine Amri <amine.elamri@gmail.com>
2022-10-23 12:48:59 +0100

    gateway

---
+++ Server/api-gateway/src/main/java/com/evoting/inscriptionservice/InscriptionServiceApplication.java
@@ -0,0 +1,21 @@
+package com.evoting.inscriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class InscriptionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InscriptionServiceApplication.class, args);
+    }
+
+}
